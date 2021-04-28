7cd46c056ec77cf0c17ef8e263124aade44f13ac
EkremOk <ekrem.okanovic@gmail.com>
2021-04-28 15:05:26 +0200

    Korigovan nacin dodjele porta applikaciji.

--- server.js
+++ server.js
@@ -38,6 +38,8 @@ app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcryp
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
-app.listen(3000, () => {
-  console.log('app is running and listening on port 3000.');
+const PORT = process.env.PORT;
+
+app.listen(PORT|| 3000, () => {
+  console.log(`app is running and listening on port ${PORT}`);
 })
